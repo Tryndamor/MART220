696980d8ab4cf1e24b9ff5313b3de7150da47a73
Trevor Kleh <klehtrevor@gmail.com>
2025-02-18 17:52:11 -0700

    Updated homework 4 - KnightClass

--- Homework 4/JS/KnightClass.js
+++ Homework 4/JS/KnightClass.js
@@ -1,43 +1,26 @@
-class KnightClass {
-    constructor(path, x, y) {
-        this.path = path;  // Image path
-        this.x = x;        // X position
-        this.y = y;        // Y position
-        this.img = null;   // To store the loaded image
-        this.loaded = false;  // To track whether the image has loaded
+class KnightClass
+{
+    constructor(path, x, y)
+    {
+        this.path = path;
+        this.x = x;
+        this.y = y;
     }
 
-    // Load and return the image
-    getImage() {
-        if (this.img === null) {
-            // Try loading the image
-            this.img = loadImage(this.path, 
-                // Success callback
-                () => {
-                    this.loaded = true;  // Image loaded successfully
-                    console.log("Image loaded successfully!");
-                }, 
-                // Error callback
-                () => {
-                    console.log("Error loading image.");
-                }
-            );
-        }
-        return this.img;  // Return the image (null if not loaded)
+
+    getImage()
+    {
+        return loadImage(this.path);
     }
 
-    // Get the X position of the knight
-    getX() {
+    getX()
+    {
         return this.x;
     }
 
-    // Get the Y position of the knight
-    getY() {
+    getY()
+    {
         return this.y;
     }
 
-    // Check if the image has been loaded
-    isLoaded() {
-        return this.loaded;
-    }
 }
