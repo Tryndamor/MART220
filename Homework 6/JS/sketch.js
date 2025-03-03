8273e3c9c8229089ec6fd8921ac881231dcbc6b7
Trevor Kleh <klehtrevor@gmail.com>
2025-03-03 13:42:35 -0700

    Homework 6 - Week 7
    1) Read images from a txt file
    2) Move food randomly at random times
    3) Keep score when food is collected
    4) Have countdown to show when game is over

---
+++ Homework 6/JS/sketch.js
@@ -0,0 +1,160 @@
+var knightFrames = [];
+var currentFrame = 0;
+var frameDelay = 5;
+var timer = 0;
+var food;
+var knightX = 400, knightY = 100, knightWidth = 100, knightHeight = 100;
+var foodWidth = 60, foodHeight = 60;
+var attackPath = [];
+var foodTimer = 0;
+var foodDelay = 120;
+var score = 0;
+var countdown = 60;
+var gameIsOver = false;
+
+function preload() 
+{
+    attackPath = loadStrings("./AnimationAssets/Attack.txt");
+    food = new FoodClass(200, 200);
+}
+
+function setup() 
+{
+    createCanvas(800, 800);
+    frameRate(60);
+    for (var i = 0; i < attackPath.length; i++)
+    {
+        knightFrames[i] = loadImage(attackPath[i]);
+    }
+    gameIsOver = false;
+}
+
+function draw() 
+{
+    if (!gameIsOver) 
+    {
+        background(200); 
+        updateAnimation();
+        updateFoodMovement(); 
+        renderFood(); 
+        renderKnightImage();  
+        knightMovement();
+        checkFoodCollision();
+        displayScore(); 
+        titleText();
+        updateCountdown(); 
+    } 
+    else 
+    {
+        displayGameOver(); 
+    }
+}
+
+function updateAnimation() 
+{
+    timer++;
+    if (timer >= frameDelay) 
+    {
+        currentFrame = (currentFrame + 1) % knightFrames.length;
+        timer = 0;
+    }
+}
+
+function updateFoodMovement()                                    // Randomize food position
+{
+    foodTimer++;
+    if(foodTimer >= foodDelay)
+    {
+        food.randomizePosition();                              
+        foodTimer = 0
+    }
+}
+
+function renderKnightImage() 
+{
+    image(knightFrames[currentFrame], knightX, knightY, knightWidth, knightHeight);
+}
+
+function knightMovement() 
+{
+    if (keyIsDown(65)) 
+    {                   // A key (left)
+        knightX -= 5;
+    }
+    if (keyIsDown(68)) 
+    {                   // D key (right)
+        knightX += 5;
+    }
+    if (keyIsDown(87)) 
+    {                   // W key (up)
+        knightY -= 5;
+    }
+    if (keyIsDown(83)) 
+    {                   // S key (down)
+        knightY += 5;
+    }
+}
+
+function renderFood() 
+{
+    food.display();
+}
+
+function checkFoodCollision()                                   // This is checking the collision of the knight to the food
+{
+    if (food.checkCollision(knightX, knightY, knightWidth, knightHeight)) 
+    {
+        food.randomizePosition();  
+        score++;
+    }
+}
+
+function displayScore()
+{
+    textSize(32);
+    fill(0,102,153);
+    textAlign(CENTER, TOP);
+    text("Score:" + score, 550,100);                            // Displays the Score of collected foods
+}
+
+function updateCountdown()                                      // Updates Countdown from 60 -> 0
+{
+    if (countdown > 0)
+    {
+        countdown -= 1 / 60;
+    }
+    else
+    {
+        countdown = 0;
+        gameIsOver = true;
+    }
+    displayCountdown();
+}
+
+function displayCountdown()
+{
+    textSize(32);
+    fill(0, 102, 153);
+    textAlign(RIGHT, TOP);
+    text("Time: " + Math.floor(countdown), 310, 100);           // Displays Countdown
+
+}
+
+function displayGameOver() 
+{
+    background(200); 
+    textSize(64);
+    fill(255, 0, 0);
+    textAlign(CENTER, CENTER);
+    text("Game Over", width / 2, height / 2);                   // Display game over message after 60 seconds
+    textSize(32);
+    text("Final Score: " + score, width / 2, height / 2 + 50);  // Displays Final Score after 60 seconds
+}
+
+function titleText() 
+{
+    textSize(32);
+    fill(0,102, 153);
+    textAlign(CENTER, TOP);
+    text("Trevor Kleh - Animated Knight", width / 2, 20);       //This is showing the Title of the game at the top
+}
